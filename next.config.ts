a5b11dcca83fbd468870075b8330ba528af1be79
Muhammed Anas <muhammedanasmp2001@gmail.com>
2026-03-08 19:50:17 +0300

    feat: Add `media.istockphoto.com` to allowed image domains in Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'static.fotor.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.istockphoto.com',
       }
     ],
   },
